69ec07d62359261a33597b494d92ec8f0eb761ed
Javonte Tuppince <taetupp47@gmail.com>
2021-04-06 14:39:21 -0400

    setting up my componets and my utils folder

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Search from './componets/Search'
+
+function App() {
+  return (
+    <Search/>
+  );
+}
+
+export default App;
---
+++ client/src/componets/Search/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import API from '../../utils/API'
+
+function Search(){
+    
+    return(
+        <h1>hello world</h1>
+    );
+};
+
+export default Search;
